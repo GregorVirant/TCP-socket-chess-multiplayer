b0767408e5868f6ade6b013fd905f29ae5767bec
UrbanLevicar <147720464+UrbanLevicar@users.noreply.github.com>
2024-11-27 16:35:37 +0100

    Persistentna povezava in več odjemalcev na strežnik
    Dodano več nitenje na strežniku in za vsakega odjemalca uporabi eno nit tako da je lahko povezanih več odjemalcev na isti strežnik. Povezava se prav tako ne prekine dokler je odjemalec ne prekine.

--- test/test_server/client.py
+++ test/test_server/client.py
@@ -1,40 +1,43 @@
 import socket
 from cryptography.fernet import Fernet
 
-SERVER_IP = "147.185.221.23"  # IP naslov strežnika
-PORT = 41818  # Vrata strežnika
-BUFFER_SIZE = 1024  # Medpomnilnik za prejemanje podatkov
+SERVER_IP = "127.0.0.1"  # IP naslov strežnika
+PORT = 1234  # Vrata strežnika
+BUFFER = 1024  # Medpomnilnik za prejemanje podatkov
 
 # Simetrično kriptiranje
-key = "B8DRpjfj4ieG6zHMs7Ydn8O02MH8ZKnIMLCRoqvxFwA="
+KEY = "B8DRpjfj4ieG6zHMs7Ydn8O02MH8ZKnIMLCRoqvxFwA="
+cipher_suite = Fernet(KEY)
 
-cipher_suite = Fernet(key)
-
-def send_message(message):
+def main():
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP/IP vtičnica
         client.connect((SERVER_IP, PORT))  # Povezava s strežnikom
-    
     except Exception as e:
         print("Napaka pri povezovanju s strežnikom:", e)
         return
-    
+    try:
+        while True:
+            message = input("Vnesite sporočilo za pošiljanje (ali 'exit' za izhod): ")
+            if message.lower() == 'exit':
+                break
+            send_message(client, message)
+    except Exception as e:
+        print("Napaka pri komunikaciji s strežnikom:", e)
+    finally:
+        client.close()
+
+def send_message(client, message):
     try:
         # Enkripcija sporočila s simetričnim ključem
         encrypted_message = cipher_suite.encrypt(message.encode('utf-8'))
         client.sendall(encrypted_message)  # Pošiljanje šifriranega sporočila strežniku
-        encrypted_response = client.recv(BUFFER_SIZE)  # Prejemanje šifriranega sporočila od strežnika
-        print(f"Prejeto šifrirano sporočilo: {encrypted_response}")
+        encrypted_response = client.recv(BUFFER)  # Prejemanje šifriranega sporočila od strežnika
+        #print(f"Prejeto šifrirano sporočilo: {encrypted_response}")
         response = cipher_suite.decrypt(encrypted_response).decode('utf-8')  # Dešifriranje sporočila
         print(f"Prejeto: {response}")
     except Exception as e:
-        print("Napaka pri komunikaciji s strežnikom:", e)
-    finally:
-        client.close() 
+        print("Napaka pri pošiljanju ali prejemanju podatkov:", e)
 
 if __name__ == "__main__":
-    while True:
-        message = input("Vnesite sporočilo za pošiljanje (ali 'exit' za izhod): ")
-        if message.lower() == 'exit':
-            break
-        send_message(message)  # Pošiljanje sporočila strežniku
+    main()  # Zagon odjemalca

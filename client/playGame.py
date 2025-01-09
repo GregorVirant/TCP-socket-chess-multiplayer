3cc9d996d8bce6a01e4c1abc7f1d771088091dc5
Gregor Virant <110791157+GregorVirant@users.noreply.github.com>
2025-01-09 01:49:44 +0100

    removed menu promotion which might cause issues

--- client/playGame.py
+++ client/playGame.py
@@ -1,7 +1,6 @@
 from gui import *
 import sendingAndReciving
 from sendingAndReciving import closeConnection,startSocket,setGameCode, send_message
-from menuPromotion import pickFigure	
 
 board=[[-2,-3,-4,-5,-6,-4,-3,-2],
         [-1,-1,-1,-1,-1,-1,-1,-1],
